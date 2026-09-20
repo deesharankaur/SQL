53f8e4d944cd39beaec6e27b2a6c06e338d69eaa
Deesharan Kaur <csai2425@gmail.com>
2026-09-20 13:09:14 +0530

    5.sql
    Add SQL query to find teams that Satchel Paige played for.

---
+++ Week1/moneyball/5.sql
@@ -0,0 +1,14 @@
+/*
+It’s a bit of a slow day in the office. Though Satchel no longer plays, in 5.sql, write a SQL query to find all teams that Satchel Paige played for.
+
+Your query should return a table with a single column, one for the name of the teams.
+*/
+
+SELECT "name" FROM "teams"
+WHERE "id" IN (
+    SELECT "team_id" FROM "performances"
+    WHERE "player_id" IN (
+        SELECT "id" FROM "players"
+        WHERE "first_name" LIKE 'Satchel%' AND "last_name" LIKE '%Paige'
+    )
+);
